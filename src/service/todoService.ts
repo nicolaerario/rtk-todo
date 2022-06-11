667bcd831cdfebbecbddbbe7adee357f61cb3bf0
Nicola Erario <nicola.erario@gmail.com>
2022-06-11 18:49:11 +0200

    feat: Add Todo component

--- src/service/todoService.ts
+++ src/service/todoService.ts
@@ -2,12 +2,12 @@ import { TodoModel } from '../models/todo.model';
 import { api } from './Api';
 
 const getAllTodos = async () => {
-  let response = await api().get('todos');
+  const response = await api().get('todos');
   return response.data;
 };
 
 const getSingleTodo = async (todo_id: number) => {
-  let response = await api().get('todos');
+  const response = await api().get('todos');
   return response.data.filter((todo: TodoModel) => todo.id === todo_id)[0];
 };
 
